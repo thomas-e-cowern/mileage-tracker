ada20bf3986b61be59ee55dd1b3bfc8ad12cbab5
Thomas E Cowern <thomas.e.cowern@gmail.com>
2020-11-15 16:08:08 -0500

    added get all and create routes

--- api/router/miles.js
+++ api/router/miles.js
@@ -2,11 +2,25 @@ const express = require('express')
 const Miles = require('../db/models/miles')
 
 const router = new express.Router()
+router.use(express.json())
 
-router.get('/', (req, res) => {
-  res.send('Hello World from mileage-tracker router')
+router.get('/miles', (req, res) => {
+  Miles.find({})
+  .then((miles) => {
+    res.send(miles)
+  }).catch((e) => {
+    console.log('error:', e)
+  })
 })
 
+router.post('/miles', (req, res) => {
+  
+  let newMiles = new Miles(req.body)
+  console.log(newMiles);
+  newMiles.save().then((miles) => {
+    res.send(miles)
+  })
+})
 
 
 module.exports = router
