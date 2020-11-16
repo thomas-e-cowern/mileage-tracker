ad7cf75388b85464e831400690a622c87e5a5f4b
Thomas E Cowern <thomas.e.cowern@gmail.com>
2020-11-15 21:54:22 -0500

    added get one route

--- api/router/miles.js
+++ api/router/miles.js
@@ -14,6 +14,23 @@ router.get('/miles', (req, res) => {
   })
 })
 
+// get one
+router.get('/miles/:id', async (req, res) => {
+  try {
+    const miles = await Miles.findById({ _id: req.params.id})
+    if (!miles) {
+      console.log('miles not found')
+      return res.send(404).send()
+    }
+
+    res.status(200).send(miles)
+
+  } catch (e) {
+    res.status(500).send()
+  }
+})
+
+// add more miles
 router.post('/miles', (req, res) => {
   let newMiles = new Miles(req.body)
   console.log(newMiles);
@@ -22,8 +39,9 @@ router.post('/miles', (req, res) => {
   })
 })
 
+// update miles entry
 router.patch('/miles/:id', (req, res) => {
-  console.log(req.param.id)
+  console.log(req.params.id)
   Miles.findByIdAndUpdate({ _id: req.params.id}, {
     $set: req.body
   }).then(() => {
@@ -34,6 +52,7 @@ router.patch('/miles/:id', (req, res) => {
   })
 })
 
+// delete miles
 router.delete('/miles/:id', (req, res) => {
   Miles.findByIdAndDelete({_id: req.params.id})
   .then(() => {
@@ -42,6 +61,5 @@ router.delete('/miles/:id', (req, res) => {
     console.log('error:', e)
   })
 })
-
 
 module.exports = router
